7944cd6e1577ce5a2d52b60e809096083752c978
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-08-28 06:44:43 -0300

    Models.py modalidades

--- Backend/interclasse/inscricoes/models.py
+++ Backend/interclasse/inscricoes/models.py
@@ -8,7 +8,17 @@ class Equipes(models.Model):
     def __str__(self):
         return self.nome
     
-    
-    
 class Inscricao(models.Model):
     nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+
+    MODALIDADES = [
+        ("FUTSAL", "Futsal"),
+        ("VOLEI", "Vôlei"),
+        ("TENIS-DE-MESA", "Tênis-de-mesa"),
+    ] 
+    modalidade = models.CharField(
+        max_length=20,
+        choices=MODALIDADES
+    )
